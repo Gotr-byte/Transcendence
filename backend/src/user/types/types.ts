347d53d6fe34a57c823bd9ea4490d9e35320fd89
bomsi39 <damfranke@gmail.com>
2023-08-30 14:42:23 +0200

    updated swagger documentation

--- backend/src/user/types/types.ts
+++ backend/src/user/types/types.ts
@@ -2,14 +2,3 @@ export type UserDetails = {
   username: string;
   email: string;
 };
-
-export type FriendRequestWhereInput = {
-  isAccepted: boolean;
-  senderId?: number;
-  receiverId?: number;
-};
-
-export type BlockedWhereInput = {
-  blockedUserId?: number;
-  blockingUserId?: number;
-};
