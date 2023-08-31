229140eb077ac66194d13d2e553d51139a0753a9
piotr biedermann <piotr.biedermann84@gmail.com>
2023-08-31 17:40:26 +0200

    changed locations of input boxes for the friend/ user settings

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,11 +1,4 @@
 import {
-  ChatIcon,
-  CheckCircleIcon,
-  EmailIcon,
-  StarIcon,
-  WarningIcon,
-} from "@chakra-ui/icons";
-import {
   Tab,
   TabList,
   TabPanels,
@@ -26,24 +19,22 @@ export default function Profile() {
     <Tabs mt="40px" p="20px" colorScheme="purple" variant="enclosed">
       <span>Profile</span>
       <TabList>
-        <Tab _selected={{ color: "white", bg: "purple.400" }}>Account Info</Tab>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Settings</Tab>
         <Tab _selected={{ color: "white", bg: "purple.400" }}>Friends</Tab>
         <Tab _selected={{ color: "white", bg: "purple.400" }}>Sent Friend Requests</Tab>
         <Tab _selected={{ color: "white", bg: "purple.400" }}>Recieved Friend Request</Tab>
       </TabList>
       <TabPanels>
         <TabPanel>  
-          {/* Updated to use dynamic userId */}
           <UpdateUser />
+          <AddFriend/>
+          <DeleteFriend/>
         </TabPanel>
         <TabPanel>
           <Friends />
-          <DeleteFriend/>
-          <AddFriend/>
         </TabPanel>
         <TabPanel>
           <SentFriendRequests/>
-          <AddFriend/>
         </TabPanel>
         <TabPanel>
           <ReceivedFriendRequests/>
