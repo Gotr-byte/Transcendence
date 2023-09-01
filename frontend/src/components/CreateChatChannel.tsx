7f5a7c40e8c46cf00a253a9ed2d3e0ae7c20f1e7
piotr biedermann <piotr.biedermann84@gmail.com>
2023-09-01 17:01:33 +0200

    added placeholder tabs

---
+++ frontend/src/components/CreateChatChannel.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+const AddFriend: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+
+  const handleAddFriend = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/friends/${username}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Friend added:', data);
+      
+    } catch (error) {
+      console.error('There was a problem adding the friend:', error);
+    }
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Enter friend's username" 
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleAddFriend}>Add Friend </button>
+    </div>
+  );
+};
+
+export default AddFriend;
