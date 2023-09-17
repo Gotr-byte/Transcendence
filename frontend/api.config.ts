a2471efa1a3743a7600a805b556d5b7185fd26dc
piotr biedermann <piotr.biedermann84@gmail.com>
2023-09-17 02:20:46 +0200

    config file cleanup

--- frontend/api.config.ts
+++ frontend/api.config.ts
@@ -1,14 +1,8 @@
 // export const protocol = "http://"
-// export const domain = "git.gundul.net"
-// 
+// "git.gundul.net"
 // import dotenv from 'dotenv';
-// 
 // dotenv.config();
-// export const apiUrl = process.env.FRONTEND_URL;
-// 
 export const protocol = "http://";
 export const domain = "localhost";
 export const backendPort = "4000";
-// 
 export const apiUrl = `${protocol}${domain}:${backendPort}`;
-// export const apiUrl = "http://localhost:4000";
