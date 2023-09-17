a658f251e3e094b3100afec8e53de90fca572475
piotr biedermann <piotr.biedermann84@gmail.com>
2023-09-17 12:48:34 +0200

    added floaty galaxy background

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,27 +1,19 @@
-import {
-  Tab,
-  TabList,
-  TabPanels,
-  TabPanel,
-  Tabs,
-} from "@chakra-ui/react";
+import { Tab, TabList, TabPanels, TabPanel, Tabs } from "@chakra-ui/react";
 import ChatUI from "../components/ChatUI";
 
 export default function Chat() {
   return (
-    <Tabs 
-    mt="40px" 
-    p="20px" 
-    colorScheme="purple" 
-    variant="enclosed"
-    bgImage='url("../../public/1920paper.jpg")'
-bgSize="100% 100%"  // This will stretch and squish your backgroun
-bgRepeat="no-repeat" // This will prevent the image from repeating
+    <Tabs
+      mt="40px"
+      p="20px"
+      colorScheme="purple"
+      variant="enclosed"
+      bgImage='url("../../public/1920paper.jpg")'
+      bgSize="100% 100%" // This will stretch and squish your backgroun
+      bgRepeat="no-repeat" // This will prevent the image from repeating
     >
       <TabList>
-        <Tab _selected={{ color: "white", bg: "purple.400" }}>
-          Chat
-        </Tab>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Chat</Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
