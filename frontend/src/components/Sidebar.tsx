e2597200ed2f7aa3af07b0bb676c118b42263cd8
piotr biedermann <piotr.biedermann84@gmail.com>
2023-09-17 10:23:23 +0200

    added new font and background image in the project

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,28 +1,35 @@
-import { AtSignIcon, ChatIcon, TriangleUpIcon } from "@chakra-ui/icons";
-import { List, ListItem, ListIcon } from "@chakra-ui/react"
+import { AtSignIcon, SunIcon, MoonIcon, ChatIcon, TriangleUpIcon } from "@chakra-ui/icons";
+import { List, ListItem, ListIcon, Box } from "@chakra-ui/react"
 import { NavLink } from 'react-router-dom';
 
 export const Sidebar = () => {
   return (
-	<List color="white" fontSize="1.2em" spacing={4}>
+	<Box
+	
+	bgSize="cover"
+	bgPosition="center"
+	bgRepeat="no-repeat"
+  >
+	<List color="darkbrown" fontSize="1.2em" spacing={4} style={{ fontFamily: "'IM Fell English SC', serif" }}>
 	  <ListItem>
 		<NavLink to="/profile">
-		<ListIcon as={AtSignIcon} color="white" />
+		<ListIcon as={SunIcon} color="lavender" style={{ fontFamily: "'IM Fell English SC', serif" }}/>
 		  Account
 		</NavLink>
 	  </ListItem>
 	  <ListItem>
 		<NavLink to="/chat">
-		<ListIcon as={ChatIcon} color="white" />
+		<ListIcon as={MoonIcon} color="lightblue" style={{ fontFamily: "'IM Fell English SC', serif" }}/>
 		  Chat
 		</NavLink>
 	  </ListItem>
 	  <ListItem>
 		<NavLink to="/game">
-		<ListIcon as={TriangleUpIcon} color="white" />
+		<ListIcon as={TriangleUpIcon} color="palegreen" style={{ fontFamily: "'IM Fell English SC', serif" }}/>
 		  Game
 		</NavLink>
 	  </ListItem>
 	</List>
+	</Box>
   )
 }
