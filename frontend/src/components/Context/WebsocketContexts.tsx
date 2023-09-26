8079e113222618bbc4a3cf2013e08f45579b8ab6
piotr biedermann <piotr.biedermann84@gmail.com>
2023-09-26 14:21:16 +0200

    added autoConnect header, added CORS credentials

--- frontend/src/components/Context/WebsocketContexts.tsx
+++ frontend/src/components/Context/WebsocketContexts.tsx
@@ -4,7 +4,9 @@ import { io, Socket } from 'socket.io-client';
 const socket = io('http://localhost:4000', {
 	query: {
 	  userId: 1, 
-	}
+	},
+	autoConnect: false,
+	withCredentials: true
   });
 export const WebsocketContext = createContext<Socket>(socket);
 export const WebsocketProvider = WebsocketContext.Provider;
