743190ddcf3acfb9e9fb0c64af3b0b0f628ccd44
piotr biedermann <piotr.biedermann84@gmail.com>
2023-09-26 14:24:43 +0200

    initial commit

--- frontend/src/components/Context/WebsocketContexts.tsx
+++ frontend/src/components/Context/WebsocketContexts.tsx
@@ -1,11 +1,8 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const socket = io('http://localhost:4000', {
-	query: {
-	  userId: 1, 
-	},
-	autoConnect: false,
+const socket = io('http://localhost:4000?userID=1', {
+	autoConnect: true,
 	withCredentials: true
   });
 export const WebsocketContext = createContext<Socket>(socket);
