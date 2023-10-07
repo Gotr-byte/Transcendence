d47fc34bd2f2079522d7d3f4e038c96b4dc2ae73
Gundul <sampah@mailbox.org>
2023-10-07 14:43:09 +0200

    timestamps testing

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -16,12 +16,13 @@ export class GameGateway implements OnGatewayDisconnect {
   ) {}
 
   private waitingUser: Socket | null;
+  private timestamp: number | null;
 
-  @SubscribeMessage('find-match')
-  async handleStartMatchmaking(
-    @ConnectedSocket() client: Socket,
-  ): Promise<void> {
-    if (this.waitingUser) {
+  @SubscribeMessage('match')
+  async handleStartMatchmaking(@ConnectedSocket() client: Socket,): Promise<void> 
+  {
+    if (this.waitingUser) 
+    {
       const opponent = this.waitingUser;
       this.waitingUser = null;
       // If there's a waiting user, match them together
@@ -30,22 +31,34 @@ export class GameGateway implements OnGatewayDisconnect {
       client.emit('matchmaking', 'opponent found!');
       opponent.emit('matchmaking', 'opponent found!');
       // createMatch(homePlayerId, awayPlayerId);
-    } else {
+    }
+    else
+    {
+      this.timestamp = Date.now();
       client.emit('matchmaking', 'waiting for opponent...');
       this.waitingUser = client;
     }
   }
-
+  
   @SubscribeMessage('abort-matchmaking')
-  async handleAbortMatchmaking(
-    @ConnectedSocket() client: Socket,
-  ): Promise<void> {
-    if (this.waitingUser === client) {
+  async handleAbortMatchmaking(@ConnectedSocket() client: Socket,): Promise<void>
+  {
+    if (this.waitingUser === client)
+    {
       this.waitingUser = null;
       client.emit('matchmaking', 'operation aborted');
     }
   }
 
+  @SubscribeMessage('hello')
+  async handleSalute(@ConnectedSocket() client: Socket,): Promise<void>
+  {
+    if (client != null)
+    {
+      client.emit('matchmaking', 'world');
+    }
+  }
+  
   handleDisconnect(@ConnectedSocket() client: Socket) {
     if (this.waitingUser === client) this.waitingUser = null;
   }
