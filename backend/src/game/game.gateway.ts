80acfbe406b05e807da8917e8abb8d3b59a5fe23
Gundul <sampah@mailbox.org>
2023-10-07 16:19:59 +0200

    add timeout-matchmaking

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -7,27 +7,28 @@ import {
 import { GameService } from './game.service';
 import { Socket } from 'socket.io';
 import { SocketService } from 'src/socket/socket.service';
+import { GameConfig } from './game.config';
 
 @WebSocketGateway({ cors: { origin: process.env.FRONTEND_URL } })
 export class GameGateway implements OnGatewayDisconnect {
   constructor(
     private readonly gameService: GameService,
-    private readonly socketSerivce: SocketService,
+    private readonly socketService: SocketService,
   ) {}
 
   private waitingUser: Socket | null;
   private timestamp: number | null;
 
-  @SubscribeMessage('match')
-  async handleStartMatchmaking(@ConnectedSocket() client: Socket,): Promise<void> 
+  @SubscribeMessage('match-random')
+  async handleStartRandomMatchmaking(@ConnectedSocket() client: Socket,): Promise<void> 
   {
     if (this.waitingUser) 
     {
       const opponent = this.waitingUser;
       this.waitingUser = null;
       // If there's a waiting user, match them together
-      const homePlayerId = this.socketSerivce.getUserId(opponent.id);
-      const awayPlayerId = this.socketSerivce.getUserId(client.id);
+      const homePlayerId = this.socketService.getUserId(opponent.id);
+      const awayPlayerId = this.socketService.getUserId(client.id);
       client.emit('matchmaking', 'opponent found!');
       opponent.emit('matchmaking', 'opponent found!');
       // createMatch(homePlayerId, awayPlayerId);
@@ -35,7 +36,7 @@ export class GameGateway implements OnGatewayDisconnect {
     else
     {
       this.timestamp = Date.now();
-      client.emit('matchmaking', 'waiting for opponent...');
+      client.emit('matchmaking', 'waiting for opponent...' + GameConfig.matchTimeout);
       this.waitingUser = client;
     }
   }
@@ -50,14 +51,19 @@ export class GameGateway implements OnGatewayDisconnect {
     }
   }
 
-  @SubscribeMessage('hello')
-  async handleSalute(@ConnectedSocket() client: Socket,): Promise<void>
+  @SubscribeMessage('timeout-matchmaking')
+  async handleTimeoutMatchmaking(@ConnectedSocket() client: Socket,): Promise<void>
   {
-    if (client != null)
+    if (!this.timestamp)
+      return;
+    const diff = Math.floor((Date.now() - this.timestamp) / 1000)
+    if (this.waitingUser === client && diff > GameConfig.matchTimeout)
     {
-      client.emit('matchmaking', 'world');
+      this.waitingUser = null;
+      client.emit('matchmaking', 'operation timed out');
     }
   }
+
   
   handleDisconnect(@ConnectedSocket() client: Socket) {
     if (this.waitingUser === client) this.waitingUser = null;
