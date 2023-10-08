6414a615beb51c156f019f5a13a35aff3acba806
piotr biedermann <piotr.biedermann84@gmail.com>
2023-10-08 19:09:13 +0200

    fixing chat issue

--- frontend/src/components/Context/WebsocketContexts.tsx
+++ frontend/src/components/Context/WebsocketContexts.tsx
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const socket = io(process.env.FRONTEND_URL + '/?userId=11')
+const socket = io(process.env.FRONTEND_URL + '/?userId=12')
 export const WebsocketContext = createContext<Socket>(socket);
 export const WebsocketProvider = WebsocketContext.Provider;
