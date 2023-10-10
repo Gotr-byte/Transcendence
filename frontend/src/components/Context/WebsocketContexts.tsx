faad6592df18ce1d1aa71654d2b679d4e1603d9d
piotr biedermann <piotr.biedermann84@gmail.com>
2023-10-10 19:33:05 +0200

    commit with known issue, sending messages to channels

--- frontend/src/components/Context/WebsocketContexts.tsx
+++ frontend/src/components/Context/WebsocketContexts.tsx
@@ -1,6 +1,8 @@
 import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 
-const socket = io(`${import.meta.env.VITE_API_URL}/?userId=12`);
+
+//TODO user id parameter must be client user
+const socket = io(`${import.meta.env.VITE_API_URL}/?userId=11`);
 export const WebsocketContext = createContext<Socket>(socket);
 export const WebsocketProvider = WebsocketContext.Provider;
