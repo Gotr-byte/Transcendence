aa0d8614d357dd605a50c7ba42b7bcf91a65efa4
piotr biedermann <piotr.biedermann84@gmail.com>
2023-10-18 13:28:31 +0200

    wip disabling

--- frontend/src/components/ControlPanel/TwoFactorAuthSetup.tsx
+++ frontend/src/components/ControlPanel/TwoFactorAuthSetup.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Switch } from '@chakra-ui/react';
+import { TwoFAComponent } from '../TwoFAComponent';
 
 const TwoFactorAuthSetup: React.FC = () => {
   const [qrCodeSrc, setQrCodeSrc] = useState<string | null>(null);
+  const [isSwitchedOn, setIsSwitchedOn] = useState<boolean>(false);
 
   useEffect(() => {
+    if (!isSwitchedOn) {
+      return; // Don't fetch if the switch is off
+    }
+
     async function fetchQrCode() {
       try {
-        // const response = await fetch(`${process.env.API_URL}/2fa/activate`, {
         const response = await fetch(`http://localhost:4000/2fa/activate`, {
           headers: {
             'Access-Control-Allow-Credentials': 'true',
@@ -34,17 +40,47 @@ const TwoFactorAuthSetup: React.FC = () => {
     }
 
     fetchQrCode();
-  }, []);
+  }, [isSwitchedOn]);
 
-  if (!qrCodeSrc) {
-    return <div>Loading...</div>;
+  async function handleFetchToggle2FAuthOff() {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/2fa/deactivate`,
+        {
+          method: "PATCH",
+          credentials: "include",
+          // 'Access-Control-Allow-Credentials': 'true',
+          // 'Access-Control-Allow-Origin': 'http://localhost:5173',
+        }
+      );
+      if (response.ok) {
+        alert("2Fauth deactivated successfully");
+      } else {
+        throw new Error("Failed to deactivate 2Fauth.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+    // window.location.reload();
   }
+
+  function handleToggle() {
+    if (isSwitchedOn === false) {
+      setIsSwitchedOn(true);
+    } else {
+      handleFetchToggle2FAuthOff();
+      setIsSwitchedOn(false);
+    }
+  }
+
 
   return (
     <div>
       <h2>Two-Factor Authentication Setup</h2>
-      <p>Scan the QR code using a 2FA app:</p>
-      <img src={qrCodeSrc} alt="QR Code" />
+      <Switch isChecked={isSwitchedOn} onChange={handleToggle} />
+      <p>{isSwitchedOn ? 'Scan the QR code using a 2FA app:' : 'Enable the switch to set up 2FA'}</p>
+      {qrCodeSrc && isSwitchedOn && <img src={qrCodeSrc} alt="QR Code" />}
+      {isSwitchedOn && <TwoFAComponent/>}
     </div>
   );
 };
