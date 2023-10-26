13dad61b8c1913324afb1c4cd8425c60d1996447
Gundul <sampah@mailbox.org>
2023-10-26 19:07:33 +0200

    not working

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -3,61 +3,74 @@ import { WebsocketContext } from '../components/Context/WebsocketContexts';
 import JoinRandom from '../components/Game/JoinRandom';
 
 interface Coordinates {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 }
 
 interface GameState {
-  paddleOne: Coordinates;
-  paddleTwo: Coordinates;
-  ball: Coordinates;
-  scoreOne: number;
-  scoreTwo: number;
+	paddleOne: Coordinates;
+	paddleTwo: Coordinates;
+	ball: Coordinates;
+	scoreOne: number;
+	scoreTwo: number;
 }
 
 const Game: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [gameState, setGameState] = useState<GameState | null>(null);
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const [gameState, setGameState] = useState<GameState | null>(null);
 
-  const socketIo = useContext(WebsocketContext);
+	const socketIo = useContext(WebsocketContext);
 
-  useEffect(() => {
-    socketIo.on('GameLoop', (data: GameState) => {
-      if (data.ball.x !== undefined && data.ball.y !== undefined) {
-        setGameState(data);
-      }
-    });
 
-    socketIo.on('disconnect', () => {
-      console.log('Socket.io connection disconnected.');
-    });
-  }, [socketIo]);
+	useEffect(() => {
+		socketIo.on('GameLoop', (data: GameState) => {
+			if (data.ball.x !== undefined && data.ball.y !== undefined) {
+				setGameState(data);
+			}
+		});
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas && gameState) {
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        // Fill the canvas with white color
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+	useEffect(() => {
+		function handleKeyDown(e) {
+			console.log(e.keyCode);
+		};
+		
+		document.addEventListener('keydown', handleKeyDown);
 
-        // Draw the ball in red
-        ctx.beginPath();
-        ctx.arc(gameState.ball.x, gameState.ball.y, 10, 0, 2 * Math.PI); // 10 is the radius of the ball
-        ctx.fillStyle = 'red';
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-  }, [gameState]);
+		return function cleanup() {
+			document.removeEventListener('keydown', handleKeyDown);
+		}
+	});
 
-  return (
-    <div>
-      		<JoinRandom />
-          <canvas ref={canvasRef} width="1200" height="720" style={{ border: '1px solid black' }}></canvas>
-    </div>
-  );
+		socketIo.on('disconnect', () => {
+			console.log('Socket.io connection disconnected.');
+		});
+	}, [socketIo]);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (canvas && gameState) {
+			const ctx = canvas.getContext('2d');
+			if (ctx) {
+				// Fill the canvas with white color
+				ctx.fillStyle = 'white';
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+				// Draw the ball in red
+				ctx.beginPath();
+				ctx.arc(gameState.ball.x, gameState.ball.y, 10, 0, 2 * Math.PI); // 10 is the radius of the ball
+				ctx.fillStyle = 'red';
+				ctx.fill();
+				ctx.closePath();
+			}
+		}
+	}, [gameState]);
+
+	return (
+		<div>
+					<JoinRandom />
+					<canvas ref={canvasRef} width="1200" height="720" style={{ border: '1px solid black' }}></canvas>
+		</div>
+	);
 }
 
 export default Game;
