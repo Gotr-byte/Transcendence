78eb5fa8a6b74e3e00c12415862a19e095c37a94
dmf39 <damfranke@gmail.com>
2023-10-31 14:34:20 +0100

    fixed small issue in module import

--- backend/src/chat/management/management.module.ts
+++ backend/src/chat/management/management.module.ts
@@ -3,9 +3,10 @@ import { ManagementService } from './management.service';
 import { ManagementController } from './management.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { ChatSharedModule } from '../shared/chat-shared.module';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
-  imports: [AuthModule, ChatSharedModule],
+  imports: [AuthModule, ChatSharedModule, SharedModule],
   controllers: [ManagementController],
   providers: [ManagementService],
   exports: [ManagementService],
