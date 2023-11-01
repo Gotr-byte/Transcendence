c4d46c542eda6f3c73018b9b0cbc018ce6343043
piotr biedermann <piotr.biedermann84@gmail.com>
2023-11-01 12:14:35 +0100

    added achievement listing to own profile

--- frontend/src/components/ControlPanel/Achievements .tsx
+++ frontend/src/components/ControlPanel/Achievements .tsx
@@ -1,57 +1,46 @@
 import { useEffect, useState } from "react";
 import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 
-interface Match {
-	id: number;
-	homePlayerId: number;
-	awayPlayerId: number;
-	winnerId: number;
+type Achievement = string;  // Simplifying the Achievement type
+
+function Achievements() {
+    const [achievements, setAchievements] = useState<Achievement[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}/users/pbiederm/achievements`, {
+            credentials: "include",
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok " + response.statusText);
+                }
+                return response.json();
+            })
+            .then((data: Achievement[]) => setAchievements(data))
+            .catch((error: Error) => setError(error.message));
+    }, []);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <Table variant="simple">
+            <Thead>
+                <Tr>
+                    <Th>Your Achievements</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {achievements.map((achievement, index) => (
+                    <Tr key={index}>
+                        <Td>{achievement}</Td>
+                    </Tr>
+                ))}
+            </Tbody>
+        </Table>
+    );
 }
 
-function Achievements () {
-	const [matches, setMatches] = useState<Match[]>([]);
-	const [error, setError] = useState<string | null>(null);
-
-	useEffect(() => {
-		fetch(`${import.meta.env.VITE_API_URL}/matches/all/user`, {
-			credentials: "include",
-		})
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Network response was not ok " + response.statusText);
-				}
-				return response.json();
-			})
-			.then((data: Match[]) => setMatches(data))
-			.catch((error: Error) => setError(error.message));
-	}, []);
-
-	if (error) {
-		return <div>Error: {error}</div>;
-	}
-
-	return (
-		<Table variant="simple">
-			<Thead>
-				<Tr>
-					<Th>ID</Th>
-					<Th>Home Player ID</Th>
-					<Th>Away Player ID</Th>
-					<Th>Winner ID</Th>
-				</Tr>
-			</Thead>
-			<Tbody>
-				{matches.map((match) => (
-					<Tr key={match.id}>
-						<Td>{match.id}</Td>
-						<Td>{match.homePlayerId}</Td>
-						<Td>{match.awayPlayerId}</Td>
-						<Td>{match.winnerId}</Td>
-					</Tr>
-				))}
-			</Tbody>
-		</Table>
-	);
-}
-
-export default Achievements ;
+export default Achievements;
