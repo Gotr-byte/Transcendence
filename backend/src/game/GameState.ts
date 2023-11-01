179099d456fcd59943188a28a9ccb9cd9a869d03
Gundul <sampah@mailbox.org>
2023-11-01 18:21:00 +0100

    fox methods

--- backend/src/game/GameState.ts
+++ backend/src/game/GameState.ts
@@ -25,10 +25,17 @@ export class Fox {
 	public isUnlocked:	boolean = false;
 	public isEvil:		boolean = false;
 	public isEnraged:	boolean = false;
+	public hasSizeOf:	number = config.fox.minSize;
+	public timeStamp:	number = 0;
+	public paddleTime:	number = 0;
+	public sticking:	number = 0;
+	public velocity:	number = config.ball.velocity;
 
 	constructor(
 		public position:	Coordinate
-	) {}
+	) { 
+		this.timeStamp= Date.now();
+	}
 }
 
 export class Paddle {
@@ -103,21 +110,203 @@ export class GameState
 		if (this.instance.hasScored())
 		{
 			this.resetGameState();
-			if (Math.random() * 100 < 50.0)
+
+			// PoC, can be removed later
+			if (Math.random() * 100 < 50)
 				this.ball2.isUnlocked = true;
 			else
 				this.ball2.isUnlocked = false;
+			// remove until here
+
 			this.instance.addRound();
 			this.winner = this.instance.whoWon();
 			if (this.winner)
 				this.instance.finishGame();
 			return;
 		}
+		this.unlockBall();
+		this.unlockFox();
+		if (this.fox.isUnlocked)
+		{
+			this.checkFoxMood();
+			this.isEnragedFox();
+			this.calcFoxPosition();
+			//this.unstickFoxFromPaddle();
+			this.freePaddles();
+
+
+		}
+
 		this.ball = this.calcBallPosition(this.ball);
 		if (this.ball2.isUnlocked)
 			this.ball2 = this.calcBallPosition(this.ball2);
 		this.calcPaddlePosition(this.paddle1);
 		this.calcPaddlePosition(this.paddle2);
+	}
+
+	public unlockBall(): void
+	{
+		if (this.instance.getScore1() + this.instance.getScore2() == config.unlockSecondBallAt)
+			this.ball2.isUnlocked = true;
+	}
+
+	public unlockFox(): void
+	{
+		if (this.instance.getScore1() + this.instance.getScore2() == config.fox.showUpWhen)
+		{
+			this.fox.isUnlocked = true;
+			this.fox.timeStamp = Date.now();
+			this.fox.paddleTime = Date.now();
+		}
+	}
+
+	public unstickFoxFromPaddle(): void
+	{
+		if (!this.fox.isUnlocked || !this.fox.isEvil)
+			return;
+		if ((Date.now() > this.fox.sticking + config.fox.timeStick2Paddle) || 
+			(Math.random() * 1000 <= 10))
+		{
+			this.fox.isEvil = false;
+			this.fox.timeStamp = Date.now();
+			this.fox.sticking = 0;
+		}
+	}
+
+	public checkFoxMood(): void
+	{
+		if (!this.fox.isUnlocked)
+			return;
+		if (Date.now() > this.fox.timeStamp + config.fox.maxTimeGoodEvil)
+		{
+			this.fox.timeStamp = Date.now();
+			if (Math.random() * 1000 > 500)
+				this.fox.isEvil = true;
+			else
+				this.fox.isEvil = false;
+		}
+	}
+
+	public isEnragedFox(): void
+	{
+		if (!this.fox.isUnlocked)
+			return;
+		let rndTime = Math.random() * config.fox.getEnragedIn;
+		if (Date.now() > this.fox.paddleTime + config.fox.getEnragedIn + rndTime)
+		{
+			this.fox.isEnraged = true;
+			this.fox.velocity = config.fox.maxVelocity;
+			this.fox.hasSizeOf = config.fox.maxSize;
+		}
+	}
+
+	public unrageFox(): void
+	{
+		if (!this.fox.isUnlocked || !this.fox.isEnraged)
+			return;
+		this.fox.isEnraged = false;
+		this.fox.velocity = config.fox.minVelocity;
+		this.fox.hasSizeOf = config.fox.minSize;
+		this.fox.isEvil = false;
+		this.fox.timeStamp = Date.now();
+	}
+
+	public foxPaddleBehavior(paddle: number): void
+	{
+		this.unrageFox();
+		if (!this.fox.isEvil)
+			this.fox.paddleTime = Date.now();
+		else
+		{
+			if (paddle == 1)
+				this.paddle1.isImmobile = true;
+			else
+				this.paddle2.isImmobile = true;
+			this.fox.velocity = 0;
+			this.fox.paddleTime = Date.now() + Math.random() * config.fox.timeStick2Paddle;
+		}
+	}
+
+	public freePaddles(): void
+	{
+		if (!this.fox.isUnlocked || this.fox.velocity > 0)
+			return;
+		if (this.fox.velocity == 0 && this.fox.paddleTime < Date.now())
+		{
+			this.fox.paddleTime = Date.now();
+			this.paddle1.isImmobile = false;
+			this.paddle2.isImmobile = false;
+			this.fox.isEvil = false;
+			this.fox.velocity = config.fox.minVelocity;
+			this.fox.timeStamp = Date.now();
+		}
+	}
+
+	public calcFoxPosition() : void 
+	{
+		let fox_new_x: number = this.fox.position.x + Math.round(this.fox.velocity * this.fox.position.x);
+		let fox_new_y: number = this.fox.position.y + Math.round(this.fox.velocity * this.fox.position.y);
+
+		if (this.fox.sticking)
+			return;
+
+		if (fox_new_y - this.fox.hasSizeOf <= 0) 
+		{
+			fox_new_y = this.fox.hasSizeOf;
+			this.fox.position.y *= -1;
+		}
+		else if (fox_new_y + this.fox.hasSizeOf >= config.game_canvas.height) 
+		{
+			fox_new_y = config.game_canvas.height - this.fox.hasSizeOf;
+			this.fox.position.y *= -1;
+		}
+		if (fox_new_x - this.fox.hasSizeOf <= 0) 
+		{
+			fox_new_x = this.fox.hasSizeOf;
+			this.fox.position.x *= -1;
+		}
+		else if (fox_new_x + this.fox.hasSizeOf >= config.game_canvas.width) 
+		{
+			fox_new_x = config.game_canvas.width - this.fox.hasSizeOf;
+			this.fox.position.x *= -1;
+		}
+		if ((fox_new_x - this.fox.hasSizeOf) <= (config.paddle.buffer + config.paddle.width)) 
+		{
+			if ((((fox_new_y - this.fox.hasSizeOf) > (this.paddle1.position.y + this.paddle1.height)) ||
+				((fox_new_y + this.fox.hasSizeOf) < this.paddle1.position.y)))
+			{
+				//do nothing
+			}
+			else
+			{
+				fox_new_x = this.paddle1.position.x + config.paddle.width + this.fox.hasSizeOf;
+				this.fox.position.y = (fox_new_y - (this.paddle1.position.y + (this.paddle1.height / 2))) / (config.paddle.height / 4);
+				this.fox.position.x *= -1;
+				this.foxPaddleBehavior(1);
+			}
+		}
+		else if (fox_new_x + this.fox.hasSizeOf >= (config.game_canvas.width - config.paddle.buffer - config.paddle.width)) 
+		{
+			if (((((fox_new_y - this.fox.hasSizeOf) > (this.paddle2.position.y + this.paddle1.height)) ||
+				(fox_new_y + this.fox.hasSizeOf) < this.paddle2.position.y)))
+			{
+				//do nothing
+			}
+			else
+			{
+				fox_new_x = this.paddle2.position.x - this.fox.hasSizeOf;
+				this.fox.position.y = (fox_new_y - (this.paddle2.position.y + (this.paddle2.height / 2))) / (config.paddle.height / 4);
+				this.fox.position.x *= -1;
+				this.foxPaddleBehavior(2);
+			}
+		}
+		// 1% chance if enraged to reverse x and/or y direction
+		if (this.fox.isEnraged && Math.random() * 1000 <= 10)
+			fox_new_x *= -1;
+		if (this.fox.isEnraged && Math.random() * 1000 <= 10)
+			fox_new_y *= -1;
+		this.fox.position.x = fox_new_x;
+		this.fox.position.y = fox_new_y;
 	}
 
 	public calcBallPosition(ball: Ball) : Ball {
@@ -190,7 +379,7 @@ export class GameState
 			this.ball2.position.y = config.game_canvas.height / 2;
 			this.ball2.direction = this.calcRandomDirection(this.instance.getRound());
 
-		//make the game faster each round
+		//make the game faster each round 
 		this.ball.velocity = config.ball.velocity + (this.instance.getRound() / 2);
 
 		this.paddle1.position.y = (config.game_canvas.height - this.paddle1.height) / 2;
