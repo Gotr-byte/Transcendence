0f9b8b9027a4373cbef13ddf0671b78fc6aa65be
piotr biedermann <piotr.biedermann84@gmail.com>
2023-11-02 13:55:02 +0100

    stats in user r profile

---
+++ frontend/src/components/Chat/UserStats.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+
+interface Stats {
+    matchesPlayed: number;
+    matchesWon: number;
+    totalPoints: number;
+}
+
+interface UserStatsProps {
+    username: string;
+}
+
+function UserStats({ username }: UserStatsProps) {
+    const [stats, setStats] = useState<Stats | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!username) return;  // Don't fetch achievements if username isn't available
+
+        fetch(`${import.meta.env.VITE_API_URL}/users/${username}/matches/stats`, {
+            credentials: "include",
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok " + response.statusText);
+                }
+                return response.json();
+            })
+            .then((data: Stats) => setStats(data))
+            .catch((error: Error) => setError(error.message));
+    }, [username]);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    if (!stats) {
+        return <div>Loading...</div>;  // Show a loading indicator or a message
+    }
+
+    return (
+        <Table variant="simple">
+            <Thead>
+                <Tr>
+                    <Th>User Stats for {username}</Th>
+                    <Th></Th>
+                    <Th></Th>
+                </Tr>
+                <Tr>
+                    <Th>Matches Played</Th>
+                    <Th>Matches Won</Th>
+                    <Th>Total Points</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                <Tr>
+                    <Td>{stats.matchesPlayed}</Td>
+                    <Td>{stats.matchesWon}</Td>
+                    <Td>{stats.totalPoints}</Td>
+                </Tr>
+            </Tbody>
+        </Table>
+    );
+}
+
+export default UserStats;
