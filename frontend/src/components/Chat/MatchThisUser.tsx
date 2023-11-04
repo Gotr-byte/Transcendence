6a0ab773ef0ed6219438ac3a949214ec6bc0ee23
piotr biedermann <piotr.biedermann84@gmail.com>
2023-11-04 19:58:54 +0100

    changes to game invite button, using navigate

--- frontend/src/components/Chat/MatchThisUser.tsx
+++ frontend/src/components/Chat/MatchThisUser.tsx
@@ -1,26 +1,37 @@
 import { WebsocketContext } from "../Context/WebsocketContexts";
-import {Button} from "@chakra-ui/react"
-import {useContext} from "react";
+import { Button } from "@chakra-ui/react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface SendDirectMessageProps {
+  username: string;
+}
 
 export const MatchThisUser: React.FC<SendDirectMessageProps> = ({
   username,
 }) => {
   const socket = useContext(WebsocketContext);
+  const navigate = useNavigate();
 
-    const redirectToGame = () => {
-        window.location.href = `/game`;
-        const dataToSend = {
-          name: username,
-          game: ""
-        };
-        socket.emit("matchThisUser", dataToSend);
+  const redirectToGame = () => {
+    // Prepare the data to send
+    const dataToSend = {
+      name: username,
+      game: ""
     };
+  
+    // Emit the socket event with the object
+    socket.emit("matchThisUser", dataToSend);
+  
+    // Redirect to the game page
+    navigate('/game');
+  };
 
-    return (
-      <Button variant="solid" size="xl" onClick={redirectToGame}>
+  return (
+    <Button variant="solid" size="xl" onClick={redirectToGame}>
       G
-     </Button>
-    );
+    </Button>
+  );
 }
 
 export default MatchThisUser;
